a2b562e3ee9307d080f19ceb4c5708af070cea17
Екатерина <katushkars@mail.ru>
2022-08-29 22:31:25 +0500

    Домашние задание 1 - выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //Задание 1.1
+        System.out.println("Задние 1.1");
+        int age=17;
+        if (age >=18) {
+            System.out.println("Поздравляю, вы совершеннолетний!!");
+        }
+        if (age <18) {
+            System.out.println("Увы, вы не совершеннолетний, вам нужно немного подождать");
+        }
+        //задание 1.2
+        System.out.println("Задние 1.2");
+        int age2=24;
+        if (age2>=7 && age2<18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (age2>=18&& age2<24) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        if (age2>=24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
+        //задание 1.3
+        System.out.println("Задние 1.3");
+        int passenger = 150;
+        if (passenger>102) {
+            System.out.println("Мест нет");
+        }
+            if (passenger>60 && passenger<=102) {
+                System.out.println("Есть стоячие места");
+            }
+        if (passenger<=60) {
+            System.out.println("Есть сидячие места");
+        }
+        }
+
     }
-}
